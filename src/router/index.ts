5ee4d51430ee06fc25f4812ef2d451790c416df9
xuyong <nhxuyong@163.com>
2024-05-15 21:34:22 +0800

    ✨ feat: 添加依赖

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Demo from '../views/Demo.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
